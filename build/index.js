90742e73fa833ad390c2dffea6d48a48d70a82c2
Danielton Paiva <53627217+danieltonpaiva@users.noreply.github.com>
2022-12-27 20:06:29 -0300

    Add files via upload

---
+++ build/index.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
+  ]
+};
+
+webpack(config, (err, stats) => {
+  if (err || stats.hasErrors()) {
+    console.error(err);
+    return;
+  }
+
+  console.log(stats.toString({
+    chunks: false,  // Makes the build much quieter
+    colors: true    // Shows colors in the console
+  }));
+});
